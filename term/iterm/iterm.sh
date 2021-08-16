40b2c1df5d4b9c188a2c6d6c0b704c60ae5cad63
Frederic Sinn <frederic.sinn@icloud.com>
2021-08-16 17:07:05 +0200

    Install iterm profile

--- term/iterm/iterm.sh
+++ term/iterm/iterm.sh
@@ -2,6 +2,8 @@
 
 script_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd -P)
 
+iterm_root=~/Library/ApplicationSupport/iTerm2
+
 function setup_colors() {
     if [[ -t 2 ]] && [[ -z "${NO_COLOR-}" ]] && [[ "${TERM-}" != "dumb" ]]; then
         NOFORMAT='\033[0m' RED='\033[0;31m'
@@ -14,15 +16,6 @@ function log_error() {
     echo -e "${RED}${1}${NOFORMAT}"
 }
 
-if ! command -v brew &> /dev/null
-then
-    cat << EOF
-brew not found. Install it using
-
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-EOF
-fi
-
 if [ "$#" -ne 1 ]
 then
     log_error "No username passed, aborting!"
@@ -31,6 +24,4 @@ fi
 
 username="${1}"
 
-"${script_dir}"/fonts/fonts.sh
-"${script_dir}"/term/iterm/iterm.sh "${username}"
-"${script_dir}"/term/fish/fish.sh
+sed "s/:ITERM_USER:/${username}/g" "${script_dir}"/iterm_profiles.json.template > "${iterm_root}"/DynamicProfiles/iterm_profiles.json
