141f40019bf0ab89e6cfcbf1f592a90bbffa0cd5
Frederic Sinn <frederic.sinn@diva-e.com>
2021-08-17 16:40:24 +0200

    Prepare for using ansible

--- install.sh
+++ install.sh
@@ -1,36 +1,19 @@
 #!/usr/bin/env bash
 
-script_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd -P)
-
-function setup_colors() {
-    if [[ -t 2 ]] && [[ -z "${NO_COLOR-}" ]] && [[ "${TERM-}" != "dumb" ]]; then
-        NOFORMAT='\033[0m' RED='\033[0;31m'
-    else
-        NOFORMAT='' RED=''
-    fi
-}
-
-function log_error() {
-    echo -e "${RED}${1}${NOFORMAT}"
-}
-
 if ! command -v brew &> /dev/null
 then
-    cat << EOF
-brew not found. Install it using
-
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-EOF
+    echo "Installing brew first..."
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+else
+    echo "Brew already installed. Skipping..."
 fi
 
-if [ "$#" -ne 1 ]
+if ! command -v ansible &> /dev/null
 then
-    log_error "No username passed, aborting!"
-    exit 1
+    echo "Installing ansible first..."
+    brew install -q ansible ansible-lint
+else
+    echo "Ansible already installed. Skipping..."
 fi
 
-username="${1}"
-
-"${script_dir}"/fonts/fonts.sh
-"${script_dir}"/term/iterm/iterm.sh "${username}"
-"${script_dir}"/term/fish/fish.sh
+ansible-playbook cli-playbook.yml
