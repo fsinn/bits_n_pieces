01543b4d6626339c2da064a4d5d51d2af64a6e96
Morathii <morathii@icloud.com>
2022-02-24 21:28:51 +0100

    Prepare brew for multi-user usage (#4)

--- install.sh
+++ install.sh
@@ -1,19 +1,22 @@
 #!/usr/bin/env bash
 
-if ! command -v brew &> /dev/null
+BREW_BIN_PREFIX="$HOME"/homebrew/bin
+
+if ! command -v "${BREW_BIN_PREFIX}"/brew &> /dev/null
 then
-    echo "Installing brew..."
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    echo "Installing brew for current user..."
+    (cd "$HOME" && git clone https://github.com/Homebrew/brew homebrew)
+    (cd "$HOME" && eval "$(homebrew/bin/brew shellenv)" && brew update --force --quiet && chmod -R go-w "$(brew --prefix)/share/zsh")
 else
     echo "Brew already installed. Skipping..."
 fi
 
-if ! command -v ansible &> /dev/null
+if ! command -v "${BREW_BIN_PREFIX}"/ansible &> /dev/null
 then
     echo "Installing ansible..."
-    brew install -q ansible ansible-lint
+    "${BREW_BIN_PREFIX}"/brew install -q ansible ansible-lint
 else
     echo "Ansible already installed. Skipping..."
 fi
 
-ansible-playbook -v -K -i hosts_local.yml "$@" cli-playbook.yml
+"${BREW_BIN_PREFIX}"/ansible-playbook -v -K -i hosts_local.yml "$@" cli-playbook.yml
